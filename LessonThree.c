5665d0e53f28ed09081e6993c1658b07d5e3ee08
unknown <daniel.snapir@gmail.com>
2025-12-19 14:54:45 +0200

    did exercises 02 and 03

--- LessonThree.c
+++ LessonThree.c
@@ -21,6 +21,70 @@ char *ft_strcpy(char *dest, char *src)
     dest[i] = src[i];
     return dest;
 }
+//Exercise 01:
+//Reproduce the behavior of the function strncpy (man strncpy).
+/*
+===========================================
+strncpy
+Copy characters from string
+Copies the first num characters of source to destination. If the end of the source C string (which is signaled by a null-character) is found before num characters have been copied, destination is padded with zeros until a total of num characters have been written to it.
+
+No null-character is implicitly appended at the end of destination if source is longer than num. Thus, in this case, destination shall not be considered a null terminated C string (reading it as such would overflow). Destination and source shall not overlap (see memmove for a safer alternative when overlapping).
+*/
+char *ft_strncpy(char *dest, char *src, unsigned int n)
+{
+    bool source_ended = false;
+    for(int i = 0; i < n; ++i)
+    {
+        if(source_ended == false)
+        {
+            dest[i] = src[i];
+        }
+        else
+        {
+            dest[i] = '0';
+        }
+        if(src[i] == '\0')
+        {
+            source_ended = true;
+        }
+    }
+    return dest;
+}
+//Exercise 02:
+//Create a function that returns 1 if the string given as a parameter contains only alphabetical characters, and 0 if it contains any other character.
+int ft_str_is_alpha(char *str)
+{
+    int index = 0;
+    int flag = 1;
+    while(str[index] != '\0')
+    {
+        char c = str[index];
+        if(!(c >= 'a' && c <= 'z') && !(c >= 'A' && c <= 'Z'))
+        {
+            return 0;
+        }
+        ++index;
+    }
+    return 1;
+}
+//Exercise 03:
+//Create a function that returns 1 if the string given as a parameter contains only digits, and 0 if it contains any other character.
+int ft_str_is_numeric(char *str)
+{
+    int index = 0;
+    int flag = 1;
+    while(str[index] != '\0')
+    {
+        char c = str[index];
+        if(!(c >= '0' && c <= '9'))
+        {
+            return 0;
+        }
+        ++index;
+    }
+    return 1;
+}
 void ft_putstr(char *str)
 {
     int length_counter = 0;
@@ -34,8 +98,19 @@ void ft_putstr(char *str)
 int main()
 {
     char source[] = "hei minun nimeni on jaakko";
-    char dest[30] = "joujouuu"; 
-    ft_strcpy(dest, source);
+    char dest[40] = "joujouuu"; 
+    char test[] = "lksajdflksjaflkjp";
+    char test2[] = "lksf9034l";
+    char test3[] = "";
+    char test4[] = "120934345943";
+    ft_strncpy(dest, source, 30);
     ft_putstr(dest);
+    printf("%d\n", ft_str_is_alpha(test));
+    printf("%d\n", ft_str_is_alpha(test2));
+    printf("%d\n", ft_str_is_alpha(dest));
+    printf("%d\n", ft_str_is_alpha(test3));
+    printf("%d\n", ft_str_is_numeric(test3));
+    printf("%d\n", ft_str_is_numeric(test4));
+    printf("%d\n", ft_str_is_numeric(test));
     return 0;
 }
