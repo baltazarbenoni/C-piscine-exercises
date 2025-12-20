9db309f4aa1638f07f9d55882bd7ec222d745671
unknown <daniel.snapir@gmail.com>
2025-12-20 21:40:45 +0200

    working on lesson 3

--- LessonThree.c
+++ LessonThree.c
@@ -21,6 +21,7 @@ char *ft_strcpy(char *dest, char *src)
     dest[i] = src[i];
     return dest;
 }
+//=======================================================
 //Exercise 01:
 //Reproduce the behavior of the function strncpy (man strncpy).
 /*
@@ -51,6 +52,7 @@ char *ft_strncpy(char *dest, char *src, unsigned int n)
     }
     return dest;
 }
+//=======================================================
 //Exercise 02:
 //Create a function that returns 1 if the string given as a parameter contains only alphabetical characters, and 0 if it contains any other character.
 int ft_str_is_alpha(char *str)
@@ -68,6 +70,7 @@ int ft_str_is_alpha(char *str)
     }
     return 1;
 }
+//=======================================================
 //Exercise 03:
 //Create a function that returns 1 if the string given as a parameter contains only digits, and 0 if it contains any other character.
 int ft_str_is_numeric(char *str)
@@ -85,6 +88,176 @@ int ft_str_is_numeric(char *str)
     }
     return 1;
 }
+//=======================================================
+//Exercise 04:
+//Create a function that returns 1 if the string given as a parameter contains only lowercase alphabetical characters, and 0 if it contains any other character.
+int ft_str_is_lowercase(char *str)
+{
+    int index = 0;
+    int flag = 1;
+    while(str[index] != '\0')
+    {
+        char c = str[index];
+        if(!(c >= 'a' && c <= 'z'))
+        {
+            return 0;
+        }
+        ++index;
+    }
+    return 1;
+}
+//=======================================================
+//Exercise 05:
+//Create a function that returns 1 if the string given as a parameter contains only uppercase alphabetical characters, and 0 if it contains any other character.
+int ft_str_is_uppercase(char *str)
+{
+    int index = 0;
+    int flag = 1;
+    while(str[index] != '\0')
+    {
+        char c = str[index];
+        if(!(c >= 'A' && c <= 'Z'))
+        {
+            return 0;
+        }
+        ++index;
+    }
+    return 1;
+}
+//=======================================================
+//Exercise 06:
+//Create a function that returns 1 if the string given as a parameter contains only printable characters, and 0 if it contains any other character.
+int ft_str_is_printable(char *str)
+{
+    int index = 0;
+    int flag = 1;
+    while(str[index] != '\0')
+    {
+        char c = str[index];
+        if(!(c >= '!' && c <= '~'))
+        {
+            return 0;
+        }
+        ++index;
+    }
+    return 1;
+}
+//=======================================================
+//Exercise 07:
+//Create a function that transforms every letter to uppercase.
+char *ft_strupcase(char *str)
+{
+    int index = 0;
+    while(str[index] != '\0')
+    {
+        char c = str[index];
+        if((c >= 'a' && c <= 'z'))
+        {
+            str[index] ^= 32;
+        }
+        ++index;
+    }
+    return str;
+}
+//=======================================================
+//Exercise 08:
+// Create a function that transforms every letter to lowercase.
+char *ft_strlowcase(char *str)
+{
+    int index = 0;
+    while(str[index] != '\0')
+    {
+        char c = str[index];
+        if((c >= 'A' && c <= 'Z'))
+        {
+            str[index] |= 0x20;
+        }
+        ++index;
+    }
+    return str;
+}
+//=======================================================
+//Exercise 09:
+//Create a function that capitalizes the first letter of each word and transforms all other letters to lowercase. A word is a string of alphanumeric characters.
+int is_alphanumeric(char c)
+{
+    if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
+    {
+        return 1;
+    }
+    return 0;
+}
+char *ft_strcapitalize(char *str)
+{
+    if(str[0] == '\0')
+    {
+        return str;
+    }
+    if((str[0] >= 'a' && str[0] <= 'z'))
+    {
+        str[0] ^= 0x20;
+    }
+    int index = 1;
+    while(str[index] != '\0')
+    {
+        char c = str[index];
+        if((c >= 'a' && c <= 'z') && !(is_alphanumeric(str[index - 1])))
+        {
+            str[index] ^= 0x20;
+        }
+        else if((c >= 'A' && c <= 'Z') && is_alphanumeric(str[index - 1]))
+        {
+            str[index] |= 0x20;
+        }
+        ++index;
+    }
+    return str;
+}
+//=======================================================
+//Exercise 10:
+//Reproduce the behavior of the function strlcpy (man strlcpy).
+
+/*
+
+*/
+unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+    if(size = 0)
+    {
+        return 0;
+    }
+    int i = 0;
+    while(src[i] != '\0')
+    {
+        if(i < size)
+        {
+            dest[i] = src[i];
+        }
+        else if(i == size)
+        {
+            dest[i] = '\0';
+        }
+        ++i;
+    }
+    return i;
+}
+//=======================================================
+//Exercise 11:
+/*
+Create a function that displays a string of characters onscreen. If this string contains characters that aren’t printable, they’ll have to be displayed in the shape of hexadecimals (lowercase), preceeded by a "backslash".
+ For example :
+    Coucou\ntu vas bien ?
+• The function should display :
+    Coucou\0atu vas bien ?
+*/
+void ft_putstr_non_printable(char *str)
+{
+
+}
+//=======================================================
+
+
+
 void ft_putstr(char *str)
 {
     int length_counter = 0;
@@ -112,5 +285,10 @@ int main()
     printf("%d\n", ft_str_is_numeric(test3));
     printf("%d\n", ft_str_is_numeric(test4));
     printf("%d\n", ft_str_is_numeric(test));
+    ft_putstr(ft_strupcase(test));
+    ft_putstr(ft_strlowcase(test));
+    ft_putstr(ft_strcapitalize(test));
+    char str[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+    ft_putstr(ft_strcapitalize(str));
     return 0;
 }
