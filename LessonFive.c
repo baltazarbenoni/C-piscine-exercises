d75b8223d8f42f67c01be5644c82f2eb1235bd92
unknown <daniel.snapir@gmail.com>
2026-01-23 15:29:22 +0200

    lesson five seems complete?? possibly review

--- LessonFive.c
+++ LessonFive.c
@@ -58,7 +58,7 @@ int ft_atoi(char *str)
         //Initial whitespace.
         if(c >= '\t' && c <= '\r' || c == ' ')
         {
-            printf("whitespace: %c\n", c);
+            //printf("whitespace: %c\n", c);
             if(num == 0)
             {
                 continue;
@@ -71,7 +71,7 @@ int ft_atoi(char *str)
         //Minus signs.
         else if(c == '-')
         {
-            printf("minus");
+            //printf("minus");
             if(num == 0)
             {
                 is_pos *= -1;
@@ -82,7 +82,7 @@ int ft_atoi(char *str)
         //Plus signs.
         else if(c == '+')
         {
-            printf("plus");
+            //printf("plus");
             if(num == 0)
             {
                 continue;
@@ -93,13 +93,13 @@ int ft_atoi(char *str)
         //Handle numbers.
         else if(c >= '1' && c <= '9')
         {
-            printf("numbers %c", c);
+            //printf("numbers %c", c);
             num *= 10;
             num += c - '0';
         }
         else if(num != 0)
         {
-            printf("exiting");
+            //printf("exiting");
             return num * is_pos;
         }
     }
@@ -122,20 +122,148 @@ arguments :
 ◦ base contains the same character twice ;
 ◦ base contains + or - ;
 */
+int ft_check_base(char* base, int len)
+{
+    //check initial conditions.
+    if(base[0] == '\0' || base[1] == '\0')
+    {
+        return 0;
+    }
+    //create array for duplicate comparison.
+    char buffer[len + 1];
+    buffer[len] = '\0';
+    int i = 0;
+    while(base[i] != '\0')
+    {
+        if(base[i] == '+' || base[i] == '-')
+        {
+            return 0;
+        }
+        if(base[i] >= '\t' && base[i] <= '\r' || base[i] == ' ')
+        {
+            return 0;
+        }
+        int j = 0;
+        while(buffer[j] != '\0')
+        {
+            if(base[i] == buffer[j])
+            {
+                return 0;
+            }
+            ++j;
+        }
+        buffer[i] = base[i];
+        ++i;
+    }
+    return 1;
+}
+char ft_get_char(int nbr, int base)
+{
+    if(base == 0 || base == 1)
+    {
+        return '0';
+    }
+    if(nbr > base)
+    {
+        printf("error, digit is %d, base is %d", nbr, base);
+        return '=';
+    }
+    if(nbr < 10)
+    {
+        char c = nbr + '0';
+        return c;
+    }
+    else
+    {
+        char c = 'A' + (char)(nbr - 10);
+        return c;
+    }
+}
 void ft_putnbr_base(int nbr, char *base)
 {
-
+    int buffer_limit = 128;
+    char buffer[buffer_limit];
+    int base_len = ft_strlen(base);
+    if(ft_check_base(base, base_len) < 1)
+    {
+        return;
+    }
+    if(nbr < 0)
+    {
+        char a = '-';
+        write(1, &a, 1);
+        nbr *= -1;
+    }
+    //get value.
+    int counter = 0;
+    while(nbr > 0)
+    {
+        int digit = (nbr % base_len);
+        char c = ft_get_char(digit, base_len);
+        buffer[counter] = c;
+        nbr /= base_len;
+        ++counter;
+        //printf("\ndigit: %d, nbr: %d, base: %d, c: %c, counter: %d\n", digit, nbr, base_len, c, counter);
+        if(counter >= buffer_limit)
+        {
+            break;
+        }
+    }
+    //print and reverse order.
+    {
+        for(int x = counter - 1; x >= 0; --x)
+        {
+            char a = buffer[x];
+            write(1, &a, 1);
+        }
+    }
+    write(1, "\n", 1);
 }
+//Exercise 05 : ft_atoi_base
+/*
+Write a function that converts the initial portion of the string pointed by str to int
+representation.
+• str is in a specific base given as a second parameter.
+• excepted the base rule, the function should work exactly like ft_atoi.
+• If there’s an invalid argument, the function should return 0. Examples of invalid
+arguments :
+◦ base is empty or size of 1;
+◦ base contains the same character twice ;
+◦ base contains + or - or whitespaces;
+• Here’s how it should be prototyped :
+*/
+int ft_atoi_base(char *str, char *base)
+{
+    int number = ft_atoi(str);
+    //printf("%d", number);
+    ft_putnbr_base(number, base);
+}
+
 int main()
 {
     char str[] = "   +++--+1934234.234.21jlsdf9293";
-    int a = ft_atoi(str);
-    ft_putnbr(a);
+    int num = ft_atoi(str);
+    ft_putnbr(num);
     ft_putnbr(-1233);
     ft_putnbr(1234);
     ft_putnbr(12345);
     ft_putnbr(-1234);
     ft_putnbr(123456);
     ft_putnbr(-123456);
+    char a = 'a';
+    a += (char)10;
+    write(1, &a, 1);
+    write(1, "\n", 1);
+    char base[] = "01";
+    char base1[] = "0123456789ABCDEF";
+    char base2[] = "sixteen";
+    char base3[] = "sixten";
+    ft_putnbr_base(124, base);
+    ft_putnbr_base(73, base);
+    ft_putnbr_base(73, base1);
+    ft_putnbr_base(160, base1);
+    ft_putnbr_base(256, base1);
+    ft_putnbr_base(73, base2);
+    ft_atoi_base(str, base1);
     return 0;
 }
