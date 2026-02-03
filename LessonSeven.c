6cb488c6a0370fa4adc878d3552ee45b386d60e1
unknown <daniel.snapir@gmail.com>
2026-02-04 01:17:06 +0200

    rnd stuff

--- LessonSeven.c
+++ LessonSeven.c
@@ -51,8 +51,8 @@ void ft_sort_params(int argc, char *argv[])
 
 int main(int argc, char *argv[])
 {
-    ft_print_program_name(argv);
+    /*ft_print_program_name(argv);
     ft_print_params(argc, argv);
-    ft_rev_params(argc, argv);
+    ft_rev_params(argc, argv);*/
     return 0;
 }
