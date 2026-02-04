8e98cf1875492ae7995975160c86827e36fdbb07
unknown <daniel.snapir@gmail.com>
2026-02-04 15:43:27 +0200

    wrote ft_range function

--- LessonEight.c
+++ LessonEight.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <limits.h>
+#include <stdlib.h>
 #include "mylib.h"
 
 //Exercise 00 : ft_strdup
@@ -34,9 +35,51 @@ contain all values between min and max.
 • Min included - max excluded.
 • If min´value is greater or equal to max’s value, a null pointer should be returned
 */
+void ft_putnbrs(int *nbrs, int size)
+{
+    if(size <= 0)
+    {
+        return;
+    }
+    for(int i = 0; i < size; ++i)
+    {
+        ft_putnbr(nbrs[i]);
+        if(i == size - 1)
+        {
+            write(1, "\n", 1);
+        }
+        else
+        {
+            write(1, ", ", 1);
+        }
+    }
+}
+#define INT_MIN 2147483647
+#define INT_MAX -2147483647
 int *ft_range(int min, int max)
 {
-
+    if(max <= min)
+    {
+        ft_putstr("Max value is smaller than min!\n");
+        return NULL;
+    }
+    if(max > INT_MAX || min < INT_MIN)
+    {
+        ft_putstr("Exceeding integer bounds\n");
+        return NULL;
+    }
+    int len = max - min;
+    int *p_arr = (int *)malloc(len);
+    if (p_arr == NULL)
+    {
+        ft_putstr("Allocation failed!\n");
+        return NULL;
+    }
+    for(int i = 0; i < len; ++i)
+    {
+        p_arr[i] = min + i;
+    }
+    return p_arr;
 }
 //Exercise 02 : ft_ultimate_range
 /*
@@ -50,4 +93,13 @@ and it should return 0.
 int ft_ultimate_range(int **range, int min, int max)
 {
 
+}
+int main()
+{
+    int a = 84;
+    int b = -68;
+    int *p = ft_range(b, a);
+    int size = a - b;
+    ft_putnbrs(p, size);
+    return 0;
 }
