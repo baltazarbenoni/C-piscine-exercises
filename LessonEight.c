1f416b0d70fad047097419d7d4515ceec7cea2f7
unknown <daniel.snapir@gmail.com>
2026-02-04 16:14:44 +0200

    fixed some bugs??

--- LessonEight.c
+++ LessonEight.c
@@ -54,8 +54,6 @@ void ft_putnbrs(int *nbrs, int size)
         }
     }
 }
-#define INT_MIN 2147483647
-#define INT_MAX -2147483647
 int *ft_range(int min, int max)
 {
     if(max <= min)
@@ -63,13 +61,12 @@ int *ft_range(int min, int max)
         ft_putstr("Max value is smaller than min!\n");
         return NULL;
     }
-    if(max > INT_MAX || min < INT_MIN)
+    long int len = max - min;
+    if(len > INT_MAX)
     {
-        ft_putstr("Exceeding integer bounds\n");
         return NULL;
     }
-    int len = max - min;
-    int *p_arr = (int *)malloc(len);
+    int *p_arr = malloc(len * sizeof(int));
     if (p_arr == NULL)
     {
         ft_putstr("Allocation failed!\n");
@@ -92,7 +89,73 @@ and it should return 0.
 */
 int ft_ultimate_range(int **range, int min, int max)
 {
+    if(max <= min)
+    {
+        *range = NULL;
+        return 0;
+    }
+    long int len = max - min;
+    if(len > INT_MAX)
+    {
+        *range = NULL;
+        return -1;
+    }
+    int *p_arr = malloc(len * sizeof(int));
+    if (p_arr == NULL)
+    {
+        *range = NULL;
+        return -1;
+    }
+    for(int i = 0; i < len; ++i)
+    {
+        p_arr[i] = min + i;
+    }
+    *range = p_arr;
+    return len;
+}
+//Exercice 03 : ft_strjoin
+/*
+Write a function that will concatenate all the strings pointed by strs separated by
+sep.
+• size is the number of strings in strs
+• if size is 0, you must return an empty string that you can free().
 
+*/
+char *ft_strjoin(int size, char **strs, char *sep)
+{
+    return sep;
+}
+//Exercise 04 : ft_convert_base
+/*
+Create a function that returns the result of the conversion of the string nbr from a
+base base_from to a base base_to.
+• nbr, base_from, base_to may be not writable.
+• nbr will follow the same rules as ft_atoi_base (from an other module). Beware of
+’+’, ’-’ and whitespaces.
+• The number represented by nbr must fit inside an int.
+• If a base is wrong, NULL should be returned.
+• The returned number must be prefix only by a single and uniq ’-’ if necessary, no
+whitespaces, no ’+’.
+*/
+char *ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+    return nbr;
+}
+//Exercise 05 : ft_split
+/*
+ Create a function that splits a string of character depending on another string of
+characters.
+• You’ll have to use each character from the string charset as a separator.
+• The function returns an array where each element of the array contains the address
+of a string wrapped between two separators. The last element of that array should
+equal to 0 to indicate the end of the array.
+• There cannot be any empty strings in your array. Get your own conclusions accordingly.
+• The string given as argument won’t be modifiable.
+*/
+char **ft_split(char *str, char *charset)
+{
+    char **p = &str;
+    return p;
 }
 int main()
 {
