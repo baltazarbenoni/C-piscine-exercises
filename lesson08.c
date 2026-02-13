64310082d04fbe23a4f4d954175268aee1957d77
unknown <daniel.snapir@gmail.com>
2026-02-14 01:31:53 +0200

    added headers to lesson08

---
+++ lesson08.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "str_utils.h"
+#include "io_utils.h"
+#include "math_utils.h"
+
+
