6072ec2bac1bf92d44a568115a79e63626b26755
unknown <daniel.snapir@gmail.com>
2026-02-17 13:57:43 +0200

    working on shell scripts and archives for lesson09

--- lesson08.c
+++ lesson08.c
@@ -105,7 +105,7 @@ int main()
     arr[1] = "World";
     arr[2] = " I ";
     arr[3] = " am ";
-    arr[4] = " aliveee$$$aliveee ";
+    arr[4] = " aliveee$$$€€€@@}]@++aliveee ";
     arr[5] = "jou";
     t_stock_str *array = ft_strs_to_tab(6, arr);
     ft_show_tab(array);
